fcce7c3fe8ef41f659763c1cdbe5ba19d8f3e44c
Jasper Bernales <bernalesjasper@gmail.com>
2018-10-03 14:29:01 +0800

    update

--- context.js
+++ context.js
@@ -1,3 +1,4 @@
+this.x = 'global x'
 function Test() {
     this.x = 1
     function Inner1() {
@@ -18,6 +19,18 @@ function Test() {
     }
 }
 const a = Test()
-a.Inner3()
-a.Inner2()
-a.Inner1()
+// a.Inner3()
+// a.Inner2()
+// a.Inner1()
+const object = {
+    x: 'Jasper',
+    test1() {
+        console.log('test1: this.x', this.x)
+    },
+    test2: () => {
+        console.log('test2: this.x', this.x)
+    }
+}
+console.log(object.test1())
+console.log(object.test2())
+console.log(this)
--- objects.js
+++ objects.js
@@ -26,21 +26,21 @@ contacts.push({
     type: 'Work',
     number: 090101930690
 })
-console.log('@my_info-2', my_info)
+// console.log('@my_info-2', my_info)
 
 
-contacts = contacts.slice(0, 1)
-console.log('@my_info-3', my_info)
+// contacts = contacts.slice(0, 1)
+// console.log('@my_info-3', my_info)
 
 
 const [primary_contact, secondary_contact] = contacts
-console.log('primary_contact: ', primary_contact)
-console.log('secondary_contact: ', secondary_contact)
+// console.log('primary_contact: ', primary_contact)
+// console.log('secondary_contact: ', secondary_contact)
 
 
 const { address: { state: province = 'Bohol', barangay = 'Tugas' } } = my_info
-console.log('barangay: ', barangay);
-console.log('province: ', province);
+// console.log('barangay: ', barangay);
+// console.log('province: ', province);
 
 
 // Object.values(address)
